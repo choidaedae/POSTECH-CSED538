1e82bae5a900ed42606d3721f0b41c60ea809d6d
Seunghun-82 <useful0512@naver.com>
2023-11-25 23:13:48 +0900

    Fix changeNetwork equal to baseline

--- model/cd_modules/baseline_attention.py
+++ model/cd_modules/baseline_attention.py
@@ -198,7 +198,7 @@ class BiSRNet(nn.Module):
         
         self.resCD = self._make_layer(ResBlock, 256, 128, 6, stride=1)
         
-        self.classifierCD = nn.Sequential(nn.Conv2d(128, 64, kernel_size=1), nn.BatchNorm2d(64), nn.ReLU(), nn.Conv2d(64, 1, kernel_size=1))
+        self.classifierCD = nn.Sequential(nn.Conv2d(128, 64, kernel_size=3, padding=1), nn.ReLU(), nn.Conv2d(64, 2, kernel_size=3, padding=1))
         initialize_weights(self.SiamSR, self.resCD, self.CotSR, self.classifierCD)
     
     def _make_layer(self, block, inplanes, planes, blocks, stride=1):
